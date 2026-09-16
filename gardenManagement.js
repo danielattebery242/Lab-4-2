b56cc27e0449b92eb63da676bcbb2293c6fcc72b
Daniel <danielattebery242@gmail.comgit config --global user name Daniel>
2026-09-16 11:01:25 -0500

    Complete functions for Lab 4-2

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,21 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+function calculateTax(amount) {
+    const taxRate = 0.08;
+    return amount * taxRate;
+}function convertToUpperCase(text) {
+    return text.convertToUpperCase();  
+}
+
+function isPalindrome(word) {
+    const cleanWord = word.toLowerCase();
+    
+    const reverseWord = cleanWord.split('').reverse().join('');
+    retucn cleanWord === reverseWord;
+}
+
+function calculateDiscountedPrice(originalPrice, discountpercentage) {
+    const disAmount = originalPrice * (discountPercentage / 100);
+    return originalPrice - discountAmount;
+}
